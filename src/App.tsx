dd4e57de90690c6402413f61d12e1482a607ec8a
Heloriel <andradematheus.dev@gmail.com>
2022-08-03 14:00:36 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="flex w-full min-h-screen items-start justify-center bg-zinc-900 text-white">
+      <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px]">
+        <img src="" alt="Profile Picture" className="rounded-full border-2 border-green-400" />
+      </div>
+      <div></div>
+    </main>
+  );
+}
+
+export default App;
