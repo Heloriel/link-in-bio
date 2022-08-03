28887fd6a933efa6756a2b651f8752101c95b2be
Heloriel <andradematheus.dev@gmail.com>
2022-08-03 15:17:04 -0300

    Username inclusion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 function App() {
   return (
-    <main className="flex w-full min-h-screen items-start justify-center bg-zinc-900 text-white">
-      <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px]">
-        <img src="" alt="Profile Picture" className="rounded-full border-2 border-green-400" />
+    <main className="flex flex-col w-full min-h-screen items-center bg-zinc-900 text-white pt-16">
+      <div className="w-">
+        <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px] mb-4">
+          <img src="https://github.com/rocketseat.png" alt="Profile Picture" className="rounded-full border-2 border-green-400" />
+        </div>
+        <div className="mb-4">
+          <span>@andradematheus</span>
+        </div>
+        <ul>
+          <li>
+            <a href="#" className="uppercase rounded-full border-2 border-green-500" target={"_blank"}>
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a href="#" className="uppercase">
+              Linkedin
+            </a>
+          </li>
+        </ul>
       </div>
-      <div></div>
     </main>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
+    fontFamily: {
+      "poppins": ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
