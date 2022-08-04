b6a336784476a4b0da0d06abb170cdee421e7323
Heloriel <andradematheus.dev@gmail.com>
2022-08-04 15:19:09 -0300

    Get links from CMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
+import { gql, useQuery } from "@apollo/client";
+import { Icons } from "./components/icons";
+
+interface PersonalLinks {
+  id: string;
+  title: string;
+  url: string;
+  icon: string;
+}
+
 function App() {
+  const GET_LINKS_QUERY = gql`
+    query GetPersonalLinks {
+      personalLinks(orderBy: order_ASC) {
+        id
+        url
+        title
+        icon
+      }
+    }
+  `;
+
+  const { data, loading } = useQuery<{ personalLinks: PersonalLinks[] }>(GET_LINKS_QUERY);
+
+  if (!data || !data.personalLinks) {
+    return <div>Vazio</div>;
+  }
+
   return (
-    <main className="flex flex-col w-full min-h-screen items-center bg-zinc-900 text-white pt-16">
-      <div className="w-">
-        <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px] mb-4">
-          <img src="https://github.com/rocketseat.png" alt="Profile Picture" className="rounded-full border-2 border-green-400" />
+    <main className="flex flex-col w-full min-h-screen items-center bg-zinc-900 text-white px-2">
+      <div className="flex items-center mt-16 flex-col w-full md:w-2/4 lg:w-1/4 bg-zinc-800 rounded-lg px-4 py-16 relative top-0">
+        <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px] mb-4 absolute top-[-50px]">
+          <img src="https://github.com/rocketseat.png" alt="Profile Picture" className="rounded-full border-4 border-violet-500" />
         </div>
         <div className="mb-4">
           <span>@andradematheus</span>
         </div>
-        <ul>
-          <li>
-            <a href="#" className="uppercase rounded-full border-2 border-green-500" target={"_blank"}>
-              Linkedin
-            </a>
-          </li>
-          <li>
-            <a href="#" className="uppercase">
-              Linkedin
-            </a>
-          </li>
+        <ul className="links-list flex flex-col items-center w-full">
+          {data.personalLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.url} target={"_blank"}>
+                  <span className="flex items-center justify-center">
+                    {link.icon ? Icons[link.icon as keyof typeof Icons] : <></>}
+                    {link.title}
+                  </span>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </main>
---
+++ src/components/icons.tsx
@@ -0,0 +1,11 @@
+import { DiscordLogo, FacebookLogo, Globe, House, InstagramLogo, Link, LinkedinLogo } from "phosphor-react";
+
+export const Icons = {
+  home: <House size={20} color="#fff" className="mr-2" />,
+  linkedin: <LinkedinLogo size={20} color="#fff" className="mr-2" />,
+  discord: <DiscordLogo size={20} color="#fff" className="mr-2" />,
+  facebook: <FacebookLogo size={20} color="#fff" className="mr-2" />,
+  web: <Globe size={20} color="#fff" className="mr-2" />,
+  link: <Link size={20} color="#fff" className="mr-2" />,
+  instagram: <InstagramLogo size={20} color="#fff" className="mr-2" />,
+};
